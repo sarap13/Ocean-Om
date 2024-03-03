ae4fc8c2846a0290d9ae5af0d6b7eed61eebe3f2
Sara Parra <131387591+sarap13@users.noreply.github.com>
2024-03-03 23:01:32 +0000

    FEAT adding dropwown and buttons to /

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -14,51 +14,82 @@ export const Home = () => {
 	const [styleStartNow, setStyleStartNow] = useState("btn-light");
 
 	// boton Learn more que cuando haya hover se ponga el texto blanco
-    const handleHoverInButton = () => {
-        setStyleStartNow("btn-outline-secondary text-light");
-    }
-    // al quitar el hover vuelva al texto en azul
-    const handleHoverOutButton = () => {
-        setStyleStartNow("btn-light");
-    };
+	const handleHoverInButton = () => {
+		setStyleStartNow("btn-outline-secondary text-light");
+	}
+	// al quitar el hover vuelva al texto en azul
+	const handleHoverOutButton = () => {
+		setStyleStartNow("btn-light");
+	};
 
 
 	return (
 		<div className="container-fluid d-flex p-0 col-lg-12 col-md-12 col-sm-12">
-			<img id="imagenFija" src="https://res.cloudinary.com/dx23woi99/image/upload/v1708541359/IMG_5841_tkuzrc.jpg" className="card-img-top p-0" alt="Imagen Fija" style={{width: "100%"}}/>
+			<img id="imagenFija" src="https://res.cloudinary.com/dx23woi99/image/upload/v1708541359/IMG_5841_tkuzrc.jpg" className="card-img-top p-0" alt="Imagen Fija" style={{ width: "100%" }} />
 			<div className="card-img-overlay">
-				
-					<div className="d-flex justify-content-end">
-						<Link to="/contactus">
-							<button type="button" className="btn btn-outline-light btn-lg p-2 mx-2">Contact Us</button>
-						</Link>
-						<Link to="/login">
-							<button type="button" className="btn btn-outline-light btn-lg">Login</button>
-						</Link>
+
+				{/* dropdown para dispositivos moviles, menu responsive */}
+				<div className="d-flex justify-content-end d-sm-flex d-md-none gap-3">
+					<div className="dropdown">
+						<button className="btn text-light btn-lg dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							Menu
+						</button>
+						<div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+							<Link to="/signup" className="dropdown-item">Signup</Link>
+							<Link to="/signup/freetrial" className="dropdown-item">Freetrial</Link>
+							<Link to="/contactus" className="dropdown-item">Contact Us</Link>
+							<Link to="/testimonials" className="dropdown-item">Testimonials</Link>
+							<Link to="/login" className="dropdown-item">Login</Link>
+						</div>
 					</div>
-		
-					<div className="mt-4 ms-2 col-lg-12 col-md-12 col-sm-12">
-						<img src={logo} className="card-img-top p-0" alt="..." style={{width: "50%"}}/>
-						
-					</div>
-				
-					<div className="d-block ms-4 text-start col-lg-12 col-md-6 col-sm-6">
-						<span className="text-light fs-5 text-center mt-3">
-						Come and practice in this ocean of online yoga <br></br> classes. 
-						With teachers from all over the globe! <br></br>
+				</div>
+				{/* Botones en línea para pantallas medianas y grandes */}
+				<div className="d-none d-sm-none d-md-flex justify-content-end gap-3">
+					<Link to="/signup">
+						<button type="button" className="btn btn-outline-light btn-lg">Signup</button>
+					</Link>
+					<Link to="/signup/freetrial">
+						<button type="button" className="btn btn-outline-light btn-lg">Freetrial</button>
+					</Link>
+					<Link to="/contactus">
+						<button type="button" className="btn btn-outline-light btn-lg p-2 mx-2">Contact Us</button>
+					</Link>
+					<Link to="/testimonials">
+						<button type="button" className="btn btn-outline-light btn-lg p-2 mx-2">Testimonials</button>
+					</Link>
+					<Link to="/login">
+						<button type="button" className="btn btn-outline-light btn-lg">Login</button>
+					</Link>
+				</div>
+
+				{/* </div> */}
+
+				<div className="mt-4 ms-2 col-lg-12 col-md-12 col-sm-12">
+					<img src={logo} className="card-img-top p-0" alt="..." style={{ width: "50%" }} />
+
+				</div>
+
+				<div className="d-block ms-4 text-start col-lg-12 col-md-6 col-sm-6">
+					<span className="text-light fs-5 text-center mt-3">
+						Come and practice in this ocean of online yoga <br></br> classes.
+						With teachers from all over the globe!
+						<br></br>
+						<br></br>
+
+						Enjoy exclusive content of yoga, meditation, and harmonium practices. <br></br>
 						Any style, at any time.
-						</span>
+					</span>
 					<div className="d-flex align-items-center justify-content-start mt-5">
 						<Link to="/sessions">
-						<button type="button" className={`btn ${styleStartNow} btn-lg text-secondary`} onMouseEnter={handleHoverInButton} onMouseLeave={handleHoverOutButton}>
-                            	Start Now
-						</button>
+							<button type="button" className={`btn ${styleStartNow} btn-lg text-secondary`} onMouseEnter={handleHoverInButton} onMouseLeave={handleHoverOutButton}>
+								Start Now
+							</button>
 						</Link>
-					</div>	
 					</div>
+				</div>
 			</div>
-		
-		
-		</div>
+
+
+		// </div >
 	);
 };
